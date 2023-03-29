1f6ac5fea6a5cfbf4db323b3057d1eacbcacfac0
theisolatedmonk <96rahulseth@gmail.com>
2023-03-29 12:23:57 +0530

    code#1 second commit

--- script.js
+++ script.js
@@ -12,6 +12,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
@@ -30,6 +31,10 @@ document.querySelector('.check').addEventListener('click', function() {
         document.querySelector('body').style.backgroundColor = '#60b347';
 
         document.querySelector('.number').style.width = '30rem';
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
 
         // Too High!
     } else if (guess > secretNumber) {
