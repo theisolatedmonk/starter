ab52817611ad9bdd2388c20019528838a70c691f
theisolatedmonk <96rahulseth@gmail.com>
2023-03-29 12:01:11 +0530

    Coding Challenge #1

--- script.js
+++ script.js
@@ -10,17 +10,28 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
-secretNumber
+
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
+
+    // No Number
     if (!guess) {
         document.querySelector('.message').textContent = 'No Number!';
+
+        // when player win 
+
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = 'Correct Number!';
+        document.querySelector('.number').textContent = secretNumber;
+
+        document.querySelector('body').style.backgroundColor = '#60b347';
+
+        document.querySelector('.number').style.width = '30rem';
+
+        // Too High!
     } else if (guess > secretNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent = 'Too High!';
@@ -43,3 +54,15 @@ document.querySelector('.check').addEventListener('click', function() {
 
     }
 })
+
+document.querySelector('.again').addEventListener('click', () => {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.message').textContent = 'Start guessing...!';
+    document.querySelector('.score').textContent = score;
+    guess = Number(document.querySelector('.guess').value = '');
+
+    document.querySelector('.number').style.width = '15rem';
+});
